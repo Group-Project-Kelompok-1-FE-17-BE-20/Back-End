d8c1e25d1b27ff0fe227495ae41382afae4e3a2a
zidan970 <amdzidan70@gmail.com>
2024-01-21 22:39:04 +0700

    add mapping model gorm to core

--- features/product/data/model.go
+++ features/product/data/model.go
@@ -56,3 +56,24 @@ func CoretoModelGorm(data []product.Core) []Product {
 
 	return productsDataGorm
 }
+
+func ModelGormToCore(data []Product) []product.Core {
+	var productsData []product.Core
+	for _, input := range data {
+		var productInput = product.Core{
+			Storage:     input.Storage,
+			RAM:         input.RAM,
+			Price:       input.Price,
+			Description: input.Description,
+			Tipe:        input.Tipe,
+			Gambar:      input.Gambar,
+			Brand:       input.Gambar,
+			Processor:   input.Processor,
+			Categories:  input.Categories,
+			Stock:       input.Stock,
+		}
+		productsData = append(productsData, productInput)
+	}
+
+	return productsData
+}
