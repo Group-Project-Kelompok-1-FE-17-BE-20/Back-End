5785209871db71c71e6e2168f60df937da97bc8f
zidan970 <amdzidan70@gmail.com>
2024-01-23 06:16:12 +0700

    moving struct user to package database

--- features/user/data/model.go
+++ features/user/data/model.go
@@ -1,30 +1,18 @@
 package data
 
 import (
+	//"Laptop/features/store"
+
+	"Laptop/app/database"
 	"Laptop/features/user"
 
 	"gorm.io/gorm"
 )
 
-type User struct {
-	gorm.Model
-	Username     string `gorm:"column:username;"`
-	NamaLengkap  string `gorm:"column:nama_lengkap;not null"`
-	Email        string `gorm:"column:email;not null;unique"`
-	Password     string `gorm:"column:password;not null"`
-	NomorHP      string `gorm:"column:nomer_hp;"`
-	Alamat       string `gorm:"column:alamat;"`
-	JenisKelamin string `gorm:"column:jenis_kelamin;"`
-	ImageProfil  string `gorm:"column:image_profil;"`
-	NamaToko     string `gorm:"column:nama_toko;"`
-	AlamatToko   string `gorm:"column:alamat_toko;"`
-	ImageToko    string `gorm:"column:image_toko;"`
-}
-
 // mapping coreUser to User
-func MapCoreUsertoUser(coreUser1 user.CoreUser) User {
+func MapCoreUsertoUser(coreUser1 user.CoreUser) database.User {
 
-	var user = &User{
+	var user = &database.User{
 		Username:     coreUser1.Username,
 		NamaLengkap:  coreUser1.NamaLengkap,
 		Email:        coreUser1.Email,
@@ -33,18 +21,14 @@ func MapCoreUsertoUser(coreUser1 user.CoreUser) User {
 		Alamat:       coreUser1.Alamat,
 		JenisKelamin: coreUser1.JenisKelamin,
 		ImageProfil:  coreUser1.ImageProfil,
-		NamaToko:     coreUser1.NamaToko,
-		AlamatToko:   coreUser1.AlamatToko,
-		ImageToko:    coreUser1.ImageToko,
-
-		Model: gorm.Model{ID: uint(coreUser1.ID)},
+		Model:        gorm.Model{ID: uint(coreUser1.ID)},
 	}
 	return *user
 
 }
 
 // Mapping User to CoreUser
-func MapUserToCoreUser(user1 User) user.CoreUser {
+func MapUserToCoreUser(user1 database.User) user.CoreUser {
 
 	coreUser := &user.CoreUser{
 		ID:           user1.ID,
@@ -56,13 +40,9 @@ func MapUserToCoreUser(user1 User) user.CoreUser {
 		Alamat:       user1.Alamat,
 		JenisKelamin: user1.JenisKelamin,
 		ImageProfil:  user1.ImageProfil,
-		NamaToko:     user1.NamaToko,
-		ImageToko:    user1.ImageToko,
-		AlamatToko:   user1.AlamatToko,
-
-		CreatedAt: user1.CreatedAt,
-		UpdatedAt: user1.UpdatedAt,
-		DeletedAt: user1.DeletedAt.Time,
+		CreatedAt:    user1.CreatedAt,
+		UpdatedAt:    user1.UpdatedAt,
+		DeletedAt:    user1.DeletedAt.Time,
 	}
 	return *coreUser
 
@@ -70,7 +50,7 @@ func MapUserToCoreUser(user1 User) user.CoreUser {
 
 // Mapping dari []User ke []CoreUser
 
-func ListMapUserToCoreUser(users []User) []user.CoreUser {
+func ListMapUserToCoreUser(users []database.User) []user.CoreUser {
 	coreUsers := make([]user.CoreUser, len(users))
 
 	for i, users := range users {
@@ -83,9 +63,6 @@ func ListMapUserToCoreUser(users []User) []user.CoreUser {
 			Alamat:       users.Alamat,
 			JenisKelamin: users.JenisKelamin,
 			ImageProfil:  users.ImageProfil,
-			NamaToko:     users.NamaToko,
-			ImageToko:    users.ImageToko,
-			AlamatToko:   users.AlamatToko,
 		}
 		coreUsers[i] = *coreUser
 	}
